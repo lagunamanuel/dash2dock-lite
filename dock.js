5c22e95826cf2d6d878957ffacbae7bf1dc6674b
lagunamanuel <lagunamanuel8@gmail.com>
2026-03-16 16:06:16 +0100

    fix: forzar el dock a la capa superior en slideIn para evitar que Firefox lo tape

--- dock.js
+++ dock.js
@@ -330,6 +330,7 @@ export let Dock = GObject.registerClass(
     }
 
     slideIn() {
+      this.container.get_parent()?.set_child_above_sibling(this.container, null);
       if (this._hidden) {
         this._hidden = false;
         this._beginAnimation();
